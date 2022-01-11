5c387effb1e32b346d920133732ff69cb0161db3
Yu Seo Jun <jigreg@naver.com>
2022-01-11 21:15:41 +0900

    RepositoryTest

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -31,6 +31,5 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
-        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
