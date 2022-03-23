9bcc426dc0366ecfb515f963857dbb9662c28af3
Yu Seo Jun <jigreg@naver.com>
2022-03-23 11:43:29 +0900

    Item Service add

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -31,8 +31,8 @@ public class Order {
 
     private LocalDateTime orderDate; //주문시간
 
-    //@Enumerated(EnumType.STRING)
-    //private OrderStatus status; // 주문상태
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태
 
     //연관관계 메서드
     public void setMember(Member member) {
@@ -47,5 +47,29 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * 주문 취소
+     */
+
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이밈 배송완료된 상품은취소가 불가능합니다.");
+        }
+
+
     }
 }
